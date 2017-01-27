9fa0313e6ae07457b74990b9fa260d795a1e64fc
Aleksey <ank@2graph.ru>
2017-01-27 17:48:44 +0300

    basics

--- app/main.js
+++ app/main.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
 // ангуляр может работать не только в браузере - но и на мобильных платформах
 // чтобы определить что мы хотим использовать ангуляр в браузере  -
@@ -13,15 +16,22 @@ var platform_browser_1 = require("@angular/platform-browser");
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 // Аннотация компонента
 var HelloAngular = (function () {
+    // добавим классу конструктор
+    // конструкторы выполняют начальную инициализацию объекта
+    // то есть при создании класса первым делом будет запущена функция конструктор
     function HelloAngular() {
+        this.name = 'Vasya'; // инициализируем ранее объявленную переменную
+        this.arrNames = ['Anton', 'Stas', 'Petya', 'Tanya'];
     }
     return HelloAngular;
-}()); // класс компонента
+}());
 HelloAngular = __decorate([
     core_1.Component({
         selector: "hello-angular",
-        template: "<div>Hello Angular 2!</div>"
+        template: "\n        <div>\n            <p>Hello {{ name }}!</p>\n            <p>{{ arrNames }}</p>\n            <ul>\n                <!-- *ngFor - \u044D\u0442\u043E \u0430\u043D\u0430\u043B\u043E\u0433 ng-repeat=\"name in arrNames\" -->\n                <li *ngFor=\"let item of arrNames\">Hello, {{ item }}</li>\n            </ul>\n        </div>\n    "
     }) // здесь точка с запятой не ставится!
+    ,
+    __metadata("design:paramtypes", [])
 ], HelloAngular);
 // соединим все модули с нашим компонентом
 // NgModule также имеет аннотацию и класс
--- app/main.ts
+++ app/main.ts
@@ -8,9 +8,29 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // Аннотация компонента
 @Component({
     selector: "hello-angular",
-    template: `<div>Hello Angular 2!</div>`
+    template: `
+        <div>
+            <p>Hello {{ name }}!</p>
+            <p>{{ arrNames }}</p>
+            <ul>
+                <!-- *ngFor - это аналог ng-repeat="name in arrNames" -->
+                <li *ngFor="let item of arrNames">Hello, {{ item }}</li>
+            </ul>
+        </div>
+    `
 }) // здесь точка с запятой не ставится!
-class HelloAngular {} // класс компонента
+class HelloAngular { // класс компонента - здесь можно добавить свойства компоненту
+    name: string; // название переменной - name, тип переменной - string
+    arrNames: string[]; // тип - массив со строками
+
+    // добавим классу конструктор
+    // конструкторы выполняют начальную инициализацию объекта
+    // то есть при создании класса первым делом будет запущена функция конструктор
+    constructor() {
+        this.name = 'Vasya'; // инициализируем ранее объявленную переменную
+        this.arrNames = ['Anton', 'Stas', 'Petya', 'Tanya'];
+    }
+}
 
 // соединим все модули с нашим компонентом
 // NgModule также имеет аннотацию и класс
